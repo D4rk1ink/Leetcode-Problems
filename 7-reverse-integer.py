f6386f509239c411e020edd52c135ce551a6c93d
Passakorn Rattanaprapan <passakorn.r@nellika.co.th>
2020-01-18 22:00:09 +0700

    #Submitted-2, 44ms, Fix in case reversed integer overflows

--- 7-reverse-integer.py
+++ 7-reverse-integer.py
@@ -1,11 +1,9 @@
 #Submitted-1, wrong answer in case reversed integer overflows
+#Submitted-2, 44ms, Fix in case reversed integer overflows
 
 class Solution:
     def reverse(self, x: int) -> int:
-        is_negative = x < 0
+        multiplier = -1 if x < 0 else 1
         str_num = str(abs(x))
-        reverse_num = str_num[::-1]
-        if is_negative:
-            return int(reverse_num) * -1
-        else:
-            return int(reverse_num)
+        reverse_num = int(str_num[::-1]) * multiplier
+        return reverse_num if reverse_num.bit_length() <= 31 else 0
